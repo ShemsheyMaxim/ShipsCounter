d805e1474ab55b216820e4af642c39cc10589c37
ShemsheyMaxim <shanhaymax@gmail.com>
2021-03-24 14:04:42 +0200

    - added methods getCountShips() and isAddedShip() to class ShipsCounter

--- src/main/java/ShipsCounter.java
+++ src/main/java/ShipsCounter.java
@@ -13,7 +13,33 @@ public class ShipsCounter {
     };
 
     public static void main(String[] args) {
-        int count = 0;
-        System.out.println(count);
+        int countShips = getCountShips(ships);
+        System.out.println("Number ships on battleField = " + countShips);
+    }
+
+    private static int getCountShips(int[][] battleField) {
+        int countShips = 0;
+        for (int coordinateY = 0; coordinateY < battleField.length; coordinateY++) {
+            for (int coordinateX = 0; coordinateX < battleField[coordinateY].length;
+                     coordinateX++) {
+                int element = battleField[coordinateY][coordinateX];
+                if (element == 1) {
+                    boolean addedShip = isAddedShip(coordinateY, coordinateX);
+                    if (!addedShip) {
+                        countShips++;
+                    }
+                }
+            }
+        }
+        return countShips;
+    }
+
+    private static boolean isAddedShip(int coordinateY, int coordinateX) {
+        boolean isAddedShip = false;
+        if (coordinateX > 0 && ships[coordinateY][coordinateX - 1] == 1
+                || coordinateY > 0 && ships[coordinateY - 1][coordinateX] == 1) {
+            isAddedShip = true;
+        }
+        return isAddedShip;
     }
 }
